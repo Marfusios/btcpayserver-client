5bb983cb91ae35cfb0bec67e15730aa70e812cf7
Mariusz Kotas <m@mkotas.cz>
2020-04-25 23:38:18 +0200

    Add console sample project

---
+++ test_integration/BTCPayServer.Client.Sample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace BTCPayServer.Client.Sample
+{
+    class Program
+    {
+        private static readonly Uri SERVER_URL = new Uri("https://your_btcpayserver");
+        private static readonly string API_KEY = "your_api_key";
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("BTCPay Server - client");
+
+            var client = new BTCPayServerClient(SERVER_URL, API_KEY);
+
+            var health = await client.GetHealth();
+        }
+    }
+}
